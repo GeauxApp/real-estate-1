25cc460058d0dbe4de34da9f9a19a425e1f42c0d
Brendt Bly <brendt.bly@outlook.com>
2017-08-26 05:23:42 -0500

    set up schema

--- server/models/estate.schema.js
+++ server/models/estate.schema.js
@@ -2,9 +2,23 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 // create the Schema
-var estateSchema = new Schema({
-   //set up schema
+var listingSchema = new Schema({
+   cost : { type: Number },
+   sqft : { type: Number },
+   city : { type: String },
+   __v : { type: Number }
 });
 
+var rentalSchema = new Schema({
+    rent : { type: Number },
+    sqft : { type: Number },
+    city : { type: String },
+    _v : { type: Number }
+});
+
+var schemaObject = {};
+schemaObject.listingSchema = mongoose.model('Listing', listingSchema);
+schemaObject.rentalSchema = mongoose.model('Rental', rentalSchema);
+
 // export our model
-module.exports = mongoose.model('Estate', estateSchema);
+module.exports = schemaObject;
