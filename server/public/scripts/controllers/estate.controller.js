44a472e301b1c06be9697ba88de4608447af071a
Brendt Bly <brendt.bly@outlook.com>
2017-08-26 12:26:18 -0500

    edit button shows input fields

--- server/public/scripts/controllers/estate.controller.js
+++ server/public/scripts/controllers/estate.controller.js
@@ -1,11 +1,19 @@
 app.controller('RealEstateController', ['EstateService', function (EstateService) {
-    console.log('RealEstateController loaded');
+    //console.log('RealEstateController loaded');
     var self = this;
+    self.editListingVisible = EstateService.editListingVisible;
 
     EstateService.getEstateData();
-    console.log('EstateService listings: ', EstateService.listings);
+    //console.log('EstateService listings: ', EstateService.listings);
     self.listings = EstateService.listings;
     self.rentals = EstateService.rentals;
 
+    self.editListing = function(listing){
+        EstateService.editListing(listing);
+    };
+    
+    self.updateListing = function(listing){
+        EstateService.updateListing(listing);
+    };
     
 }]);
--- server/public/scripts/services/estate.service.js
+++ server/public/scripts/services/estate.service.js
@@ -1,10 +1,12 @@
 app.service('EstateService', ['$http', function ($http) {
-    console.log('Estate service loaded.');
+    //console.log('Estate service loaded.');
     var self = this;
     self.listings = { list: [] };
     self.rentals = { list: [] };
     self.newListing = {};
     self.newRental = {};
+    self.editListingVisible = {};
+    
     ////https://stackoverflow.com/questions/35517899/update-table-row-data-in-angular
 
     self.getEstateData = function () {
@@ -12,20 +14,25 @@ app.service('EstateService', ['$http', function ($http) {
             method: 'GET',
             url: '/realEstate'
         }).then(function (response) {
-            console.log('response from get: ', response);
+           console.log('response from get: ', response);
             
             self.listings.list = response.data.listings;
             self.rentals.list = response.data.rentals;
-            console.log('rentals ', self.rentals);
+            //console.log('rentals ', self.rentals);
+            //make mirror table to store id:true/false where true = visible.  
+            //This is used to show / hide the input boxes when edit button is clicked for listings and rentals
+            for (var i = 0; i < self.listings.length; i++) {
+                self.editListingVisible[self.listings[i].id] = false;
+              }
         });
     }
 
     self.addListing = function () {
-        console.log('addListing called client.js, newListing: ');
-        console.log(self.newListing);
+       // console.log('addListing called client.js, newListing: ');
+       // console.log(self.newListing);
         
         $http.post('/realEstate/listing', self.newListing).then(function (response) {
-            console.log(' post response: ', response);
+            //console.log(' post response: ', response);
             self.getEstateData();
         });
     };
@@ -33,9 +40,22 @@ app.service('EstateService', ['$http', function ($http) {
     self.addRental = function () {
         //console.log('addListing called client.js');
         $http.post('/realEstate/rental', self.newRental).then(function (response) {
-            console.log(' post response: ', response);
+            //console.log(' post response: ', response);
             self.getEstateData();
         });
+    };
+
+ 
+
+    
+    self.editListing = function(listing){
+        self.editListingVisible[listing._id] = true;
+        console.log('editListingVisible: ', self.editListingVisible);
+    };
+    
+    
+    self.updateListing = function(listing){
+        self.editListingVisible[listing._id] = false;
     };
 
 }]);
